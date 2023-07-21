9491a1f4a37ea255bab4408fe2eadfa56421a8aa
NFerracuti <npferracuti@gmail.com>
2023-07-21 18:19:53 -0400

    start framework for login query

--- db/queries/loginquery.js
+++ db/queries/loginquery.js
@@ -1,11 +1,23 @@
 const db = require('../connection');
 
-const loginFunction = () => {
-  return db.query('SELECT * FROM users;')
+const loginUser = (user_email, password) => {
+  const emailCheck = `SELECT * FROM users
+  WHERE email LIKE $1;`;
+  const passCheck = `SELECT * FROM users
+  WHERE password LIKE $2;`;
+
+  console.log('email ', emailCheck);
+  console.log('password ', passCheck);
+
+  return db.query(emailCheck, [user_email, password])
     .then(data => {
+      console.log(data.rows);
       return data.rows;
-    });
-};
+    })
+    .catch((err) => {
+      return err});
+  // return db.query(`SELECT * FROM users
+  // // WHERE email LIKE $1;`)
+  };
 
-
-module.exports = { getUsers };
+module.exports = { loginUser };

5083c130f6092de88461dfd8a77f462763d98082
NFerracuti <npferracuti@gmail.com>
2023-07-21 18:19:53 -0400

    start framework for login query

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(
 app.use(express.static('public'));
 
 // requiring db queries:
-const { registerNewUser } = require('./db/queries/registerNewUser.js')
+const { registerNewUser } = require('./db/queries/registerNewUser.js');
+const { loginUser } = require('./db/queries/loginquery.js');
 
 /*
 -------------------------------------------------
@@ -174,30 +175,27 @@ app.post("/register", (req, res) => {
 // login page submission post
 //---------------------------
 app.post("/login", (req, res) => {
-  // use the userlookup function to grab the user id associateed with email
   const inputEmail = req.body.email;
   const inputPassword = req.body.password;
-  const userObj = userLookup(users, "email", inputEmail);
   const password = req.body.password;
-  const hashedPassword = userObj.hashedPassword;
 
   // edge case for logging in with a non-registered email
-  if (!userLookup(users, "email", inputEmail)) {
-    res.status(404).send("404: No user registered under that email.");
+  // if (!userLookup("email", inputEmail)) {
+  //   res.status(404).send("404: No user registered under that email.");
 
   // for wrong password
-  } else if (!bcrypt.compareSync(password, hashedPassword)) {
-    res
-    .status(401)
-    .send("401: Wrong password!");
+  // } else if (!bcrypt.compareSync(password, hashedPassword)) {
+  //   res
+  //   .status(401)
+  //   .send("401: Wrong password!");
 
-  } else {
-    const id = userObj.id;
-    req.session.user_id = id;
+  // } else {
+  //   const id = userObj.id;
+  //   req.session.user_id = id;
     res
     .status(201)
     .redirect(301, '/');
-  };
+  // };
 });
 
 // login button in header

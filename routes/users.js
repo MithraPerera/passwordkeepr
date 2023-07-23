a461e5eda486e73f295fb8aaa7f3d0faaf8e9432
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-23 18:53:46 -0400

    Render app page for the logged in user

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,24 @@ const express = require('express');
 const router  = express.Router();
 const loginUserdb = require("../db/queries/loginUser");
 
-router.get('/', (req, res) => {
-  res.render('users');
+router.get("/", (req, res) => {
+  const user = req.session.user;
+  if (!user) {
+    return res.send({ message: "not logged in" });
+  }
+
+  loginUserdb
+    .getOrganizationsByUser(user.id)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "no user with that id" });
+      }
+      const templateVars = {
+        user: req.session.user
+      };
+      res.render('users', templateVars);
+    })
+    .catch((e) => res.send(e));
 }); 
 
 module.exports = router;

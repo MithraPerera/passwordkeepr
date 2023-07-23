3c832d9c2319ea85419d9f5f031a1621daaa597e
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-23 18:22:36 -0400

    Handle login POST request and redirect user to app

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const router  = express.Router();
-const bcrypt = require("bcryptjs");  // for hashing passwords
 
-const loginUser = require("../db/queries/loginUser");
+// Import requirements
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const loginUserdb = require("../db/queries/loginUser");
 const users = require('./register');
 
-// new route to login page
+// New route to login page
 router.get('/', (req, res) => {
   if (req.session.user) {
     res.redirect('/users');
@@ -17,34 +18,30 @@ router.get('/', (req, res) => {
   }
 });
 
-
-// login page submission post
-//---------------------------
-router.post("/login", (req, res) => {
-  // use the userlookup function to grab the user id associateed with email
-  const inputEmail = req.body.email;
-  const inputPassword = req.body.password;
-  const userObj = userLookup(users, "email", inputEmail);
+// Log a user in
+router.post("/", (req, res) => {
+  const email = req.body.email;
   const password = req.body.password;
-  const hashedPassword = userObj.hashedPassword;
 
-  // edge case for logging in with a non-registered email
-  if (!userLookup(users, "email", inputEmail)) {
-    res.status(404).send("404: No user registered under that email.");
+  // Call the loginUser function to retrieve the user based on email and password
+  loginUserdb.loginUser(email, password)
+    .then((user) => {
+      if (!user) {
+        return res.status(401).send({ error: "Invalid login credentials" });
+      }
+      // Compare the password provided by the user with the hashed password from the database
+      // if (!bcrypt.compareSync(password, user.user_password)) {
+      //   return res.status(401).send({ error: "Invalid login credentials" });
+      // }
 
-  // for wrong password
-  } else if (!bcrypt.compareSync(password, hashedPassword)) {
-    res
-    .status(401)
-    .send("401: Wrong password!");
-
-  } else {
-    const id = userObj.id;
-    req.session.user_id = id;
-    res
-    .status(201)
-    .redirect(301, '/urls');
-  };
+      req.session.user = user;
+      res.redirect('/users');
+      //return res.redirect(`/organizations?user_id=${user.id}`);
+    })
+    .catch((err) => {
+      console.error('Error during login:', err);
+      return res.status(500).send('Server error during login');
+    });
 });
 
 

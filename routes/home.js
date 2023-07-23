9d276ae78fb78a82ef46e6cd8c5aad65db86f625
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-23 17:20:39 -0400

    Store logged in user information during session

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home');
+  // Retrieve user data from the session and store it in the templateVars object
+  const templateVars = {
+    user: req.session.user
+  };
+
+  // Render the "index" view with the templateVars data as the context
+  res.render('index', templateVars);
 });
 
 module.exports = router;

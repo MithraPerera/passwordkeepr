3155338bd363f1178362176c2c5a9a278ccb7392
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-25 01:24:34 -0400

    Add new seed data to tables to get more information

--- db/seeds/04_users.sql
+++ db/seeds/04_users.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (first_name, last_name, email, user_password)
-VALUES ('Alice', 'Smith', 'Alice@outlook.com', '123');
+VALUES ('Alice', 'Smith', 'alice@mail.com', '123');
 
 INSERT INTO users (first_name, last_name, email, user_password)
-VALUES ('Peter', 'Williamson', 'peter@hotmail.com', '321');
+VALUES ('Peter', 'Williamson', 'peter@mail.com', '123');
 
 INSERT INTO users (first_name, last_name, email, user_password)
-VALUES ('Rick', 'Sanchez', 'rick@msn.com', '143');
+VALUES ('Rick', 'Sanchez', 'rick@mail.com', '123');
 
 INSERT INTO users (first_name, last_name, email, user_password)
-VALUES ('Sally', 'Mercko', 'sally@gmail.com', '351');
+VALUES ('Sally', 'Mercko', 'sally@mail.com', '123');

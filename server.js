3e6d0a8fc30180158b9f198caf148e8797bfd95b
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-25 14:11:48 -0400

    Add user route to server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs"); 
 const cookieSession = require("cookie-session");
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -23,7 +23,7 @@ app.use(
   sassMiddleware({
     source: __dirname + '/styles',
     destination: __dirname + '/public/styles',
-    isSass: false,
+    isSass: false, 
   })
 );
 
@@ -38,12 +38,14 @@ app.use(cookieSession({
   keys: ['midterm'],
 }));
 
+const usersRoutes = require('./routes/users');
 const homeRoute = require('./routes/home');
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
 const logoutRoute = require('./routes/logout');
 
 // Define routes for different URL paths
+app.use('/users', usersRoutes);
 app.use('/home', homeRoute);
 app.use('/register', registerRoute);
 app.use('/login', loginRoute);
@@ -57,6 +59,41 @@ app.get('/', (req, res) => {
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
+});
+
+// Handle POST request to "/registerbutton" and redirect to "/register"
+app.post("/registerbutton", (req, res) => {
+  res.redirect(`/register`);
+});
+
+// Handle GET request to "/register" and render the "register" page
+app.get("/register", (req, res) => {
+  const user_id = req.session["user_id"];
+  const user = users[user_id];
+  const templateVars = {
+    user,
+  };
+  if (user_id) {
+    res.redirect("/");
+  } else {
+    res.render("register", templateVars);
+  }
+});
+
+// Handle POST request to "/register" and register a new user
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const id = 101;
+
+  const newUser = registerNewUser(firstName, lastName, email, password)
+    .then((data) => {
+      console.log("data params", data)
+    })
+  console.log(newUser);
 });
 
 // Handle POST request to "/logout" and clear the session cookies, then redirect to "/home"

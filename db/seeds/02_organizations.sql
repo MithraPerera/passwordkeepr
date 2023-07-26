b5d498fabbfb404082f669119c43158e9e1dc18f
Mithra Perera <mithraperera@hotmail.com>
2023-07-25 22:46:03 -0400

    seed files updated and routes cleared up

--- db/seeds/02_organizations.sql
+++ db/seeds/02_organizations.sql
@@ -8,23 +8,13 @@
 -- ('Manulife', 'https://www.manulife.ca/', '3');
 
 INSERT INTO organizations (name, org_website, admin_id)
-VALUES ('Lighthouse Labs', 'https://www.lighthouselabs.ca/', 1),
-       ('Enbridge', 'https://enbridge.ca/', 1),
-       ('Manulife', 'https://www.manulife.ca/', 1),
-       ('Health Solutions', 'https://healthsolutions.com/', 1);
+VALUES ('Lighthouse Labs', 'https://www.lighthouselabs.ca/', 1);
 
 INSERT INTO organizations (name, org_website, admin_id)
-VALUES ('Tech Corp', 'https://techcorp.com/', 2),
-       ('Enbridge', 'https://enbridge.ca/', 2),
-       ('Manulife', 'https://www.manulife.ca/', 2),
-       ('Lighthouse Labs', 'https://www.lighthouselabs.ca/', 2);
+VALUES ('Tech Corp', 'https://techcorp.com/', 2);
 
 INSERT INTO organizations (name, org_website, admin_id)
-VALUES ('Tech Corp', 'https://techcorp.com/', 3),
-       ('Lighthouse Labs', 'https://www.lighthouselabs.ca/', 3);
+VALUES ('Enbridge', 'https://enbridge.ca/', 3);
 
 INSERT INTO organizations (name, org_website, admin_id)
-VALUES ('Tech Corp', 'https://techcorp.com/', 2),
-       ('Enbridge', 'https://enbridge.ca/', 2),
-       ('Manulife', 'https://www.manulife.ca/', 2),
-       ('Lighthouse Labs', 'https://www.lighthouselabs.ca/', 2);
+VALUES ('Manulife', 'https://www.manulife.ca/', 4);

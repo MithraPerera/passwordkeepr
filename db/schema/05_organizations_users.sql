e64c6e8c8efc44106adc2e15863a7d27ea16b6b8
Madiha Waqar <90156331+madiha-waqar@users.noreply.github.com>
2023-07-26 22:04:42 -0400

    Delete 05_organizations_users.sql
    Not required according to new schema setup decided.

--- db/schema/05_organizations_users.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS organizations_users CASCADE;
-
-CREATE TABLE organizations_users (
-  organization_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  PRIMARY KEY(organization_id, user_id)
-);

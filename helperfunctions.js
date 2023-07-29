49d3369e0eb43f33254a59710c6bd53ae1ca4c9d
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-28 22:02:56 -0400

    Revert "Remove files not required"
    This reverts commit 58ecb53639b77280a163aa3397bb4958965d9fa4.

---
+++ helperfunctions.js
@@ -0,0 +1,29 @@
+const{ getUsers } = require('./db/queries/users.js');
+
+// function to generate random string of 6 characters for user id
+const generateCustomPassword = function() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let counter = 0;
+  while (counter < 7) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    counter += 1;
+  }
+  return result;
+};
+
+// *** refactor this to use sql query and check if user exists in database
+const userLookup = function(key, value) {
+  const gettingUsers = getUsers().then(data => {
+    return data
+  });
+  console.log('is this the console.log', gettingUsers);
+  // for (let i in users) {
+  //   if (users[i][key] === value) {
+  //     return users[i];
+  //   }
+  // }
+  return null;
+};
+
+module.exports = { copyToClipboard, generateRandomString, userLookup };
